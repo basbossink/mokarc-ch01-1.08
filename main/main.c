2b312a21e09cb1ab51736fa87319ecb0b336ab7f
Bas Bossink <bas.bossink@gmail.com>
2017-06-30 22:53:54 +0200

    Implement main completing the exercise.

--- main/main.c
+++ main/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../lib/character-counter.h"
+
 int
 main(int argc, char **argv) {
   (void)argc;
   (void)argv;
+  struct count_result whitespace[] = {
+    { .character = ' ', .count = 0 },
+    { .character = '\t', .count = 0 },
+    { .character = '\n', .count = 0 },
+  };
+  size_t number_of_results = sizeof(whitespace) / sizeof(struct count_result);
+  count_characters(getchar, whitespace, number_of_results);
+  printf(
+    "Standard in contains the following whitespace counts\n"    \
+    "Spaces | Tabs   | Newlines |\n"\
+    "%6lu | %6lu | %8lu |\n", whitespace[0].count, whitespace[1].count, whitespace[2].count);
   return EXIT_SUCCESS;
 }
